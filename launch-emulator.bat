fce1c5e9695f6194563c8bf2963257836f569806
Ntwari Clarance Liberiste <60586899+claranceliberi@users.noreply.github.com>
2021-10-11 21:26:57 +0200

    Create launch-emulator.bat

---
+++ launch-emulator.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+:: This CMD script start android emulator
+TITLE Launch emulator
+
+ECHO Starting android emulators
+
+ECHO =========================
+
+flutter emulators --launch Pixel_4_API_30   
+
+PAUSE
