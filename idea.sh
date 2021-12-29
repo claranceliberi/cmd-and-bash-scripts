bd3d3fc3184a8ce1c68a1cf154be2a5c3914e1e8
claranceliberi <liberintwari@gmail.com>
2021-12-29 10:55:53 +0200

    feat: idea install command in wsl

---
+++ idea.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# installation of the idea IDE
+# https://nirisarri.github.io/2020-11-03-how-to-install-and-run-intellij-idea-for-linux-in-wsl2/
+
+/opt/idea-IU-202.7660.26/bin/idea.sh
